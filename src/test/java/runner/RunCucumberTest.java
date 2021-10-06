f9d5d19f65186d2ccf4540946f2ba43d18225869
Mick Hill <mickhill@hotmail.com.br>
2021-10-06 20:33:05 -0300

    Atualizando Projeto

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -1,30 +1,43 @@
 package runner;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features",	// Path: features = "classpath:features"
-		glue = "steps",					// Path: Steps
-		monochrome = true,							// Cores no Terminal default: false
-		snippets = SnippetType.CAMELCASE,			// METHODOS do Steps em CAMELCASE
-		strict = true,								// Considerar steps indefinidos erros ou nao, default: false
-		dryRun = false,								// Validar Steps sem executar o teste
-		
-		plugin = {		// Plugins do Cucumber
-			"pretty",
-			"html:target/cucumber-reports/reports-html",
-			"json:target/cucumber-reports/report.json",
-			"junit:target/cucumber-reports/report.xml"
-		},
-		
-		tags = {		// tags = "@TagQueSeraExecutada" ou {"~@ignore"}
-			"@pesquisaGoogle"
-		}
+        features = "src/test/resources/features",	// Path: features = "classpath:features"
+        glue = "steps_definition",					// Path: Steps
+        monochrome = true,							// Cores no Terminal default: false
+        snippets = SnippetType.UNDERSCORE,			// METHODOS do Steps em CAMELCASE
+        strict = true,								// Considerar steps indefinidos erros ou nao, default: false
+        dryRun = false,								// Validar Steps sem executar o teste
+        plugin = {									// Plugins Cucumber para possivel integracao com Jenkins
+                "pretty",
+                //"html:target/cucumber-reports",
+                "junit:target/cucumber-reports/cucumber.xml",
+                "json:target/cucumber-reports/cucumber.json"
+        }
+        //,tags = { "@tagScenario" }
+
+        /**
+         * Executar testes no termial
+         *
+         *	$ mvn test
+         *	$ mvn test -Dcucumber.options="--tags @tagScenario"
+         *
+         *	$ mvn clean
+         *	$ mvn clean test
+         *
+         *
+         * Gerar Relatorio Cucumber localmente
+         *
+         *	$ mvn verify -DskipTests
+         *	$ mvn test verify
+         *	$ mvn clean test verify
+         *	$ mvn clean test -Dcucumber.options="--tags @tagScenario" verify
+         * */
 )
 
 public class RunCucumberTest
